b9e43c5b89706e91dfdf0b8ea5dcbd690eaf6df4
Matthew <wonderzombie@gmail.com>
2012-02-25 23:27:50 -0800

    Migrate IrcConn into its own file.
    Attempting some interesting ways to automatically generate commands
    using a map, fmt, and variadic args. Not having much luck.

--- gobot/main.go
+++ gobot/main.go
@@ -2,114 +2,14 @@
 package main
 
 import (
-	"bufio"
 	"fmt"
 	"flag"
-	// "io"
-	"log"
 	"net"
 	"strings"
 	"time"
 	irclib "youandmeandirc"
 )
 
-
-// IrcConn represents a connection to an IRC server.
-type IrcConn struct {
-	conn net.Conn
-	reader *bufio.Reader
-
-	host, port string
-
-	nick string
-	pass string
-	realname string
-	username string
-}
-
-/// Network-related.
-
-func (irc *IrcConn) Connect(c net.Conn) {
-	irc.conn = c
-	irc.reader = bufio.NewReader(irc.conn)
-	irc.register()
-	return
-}
-
-// send transmits a command to the currently connected server.
-func (irc IrcConn) send(s string) (int, error) {
-	fmt.Println("=>", s)
-	return fmt.Fprintln(irc.conn, s)
-}
-
-// register sends the PASS, NICK, and USER commands.
-func (irc IrcConn) register() {
-	messages := []string {
-		irc.newPassMsg(irc.pass),
-		irc.newNickMsg(irc.nick),
-		irc.newUserMsg(irc.username, irc.realname),
-	}
-
-	for _, m := range messages {
-		if _, err := irc.send(m) ; err != nil {
-			log.Println("Error:", err)
-		}
-	}
-}
-
-/// Composing various kinds of messages.
-
-func (irc IrcConn) newPassMsg(pass string) string {
-	return fmt.Sprintf("PASS %v", pass)
-}
-
-func (irc IrcConn) newNickMsg(nick string) string {
-	return fmt.Sprintf("NICK %v", nick)
-}
-
-func (irc IrcConn) newUserMsg(username, realname string) string {
-	return fmt.Sprintf("USER %v * * :%v", username, realname)
-}
-
-func (irc IrcConn) newJoinMsg(channel string) string {
-	return fmt.Sprintf("JOIN %v", channel)
-}
-
-func (irc IrcConn) newPongMsg(daemon string) string {
-	return fmt.Sprintf("PONG %v", daemon)
-}
-
-/// Public methods.
-
-func (irc IrcConn) Say(channel, chat string) (int, error) {
-	msg := fmt.Sprintf("PRIVMSG %v :%v", channel, chat)
-	return irc.send(msg)	
-}
-
-func (irc IrcConn) Join(channel string) (int, error) {
-	cmd := fmt.Sprintf("JOIN %v", channel)
-	return irc.send(cmd)	
-}
-
-func (irc IrcConn) Read() (m *irclib.IrcMessage, err error) {
-	s, err := irc.reader.ReadString('\n')
-	if err != nil {
-		log.Println("Error:", err)
-	}
-
-	m, err = irclib.ParseMessage(s)
-	if err != nil {
-		log.Println("Error:", err)
-	}
-	
-	return
-}
-
-func (irc IrcConn) Pong(daemon string) (int, error) {
-	// FIXME: shouldn't this be handled automatically?
-	cmd := irc.newPongMsg(daemon)
-	return irc.send(cmd)
-}
 
 // Bot layer.
 func hasMyName(msg string) bool {
@@ -136,14 +36,16 @@ func main() {
 	addr := net.JoinHostPort(*host, *port)
 	timeout, _ := time.ParseDuration("1m")
 
-	irc := &IrcConn{
-		nick: *nick,
-		pass: *pass,
-		username: *username,
-		realname: "...",
+	irc := &irclib.IrcConn{
+		Nick: *nick,
+		Pass: *pass,
+		Username: *username,
+		Realname: "...",
 	}
+	// TODO(wonderzombie): Fix this so that IrcConn takes a closure, or something
+	// which can generate net.Conn items for it.
 	conn, _ := net.DialTimeout("tcp", addr, timeout)
-	defer conn.Close() // FIXME: should the IrcConn take ownership?
+	defer conn.Close()
 	irc.Connect(conn)
 
 	joined := false
