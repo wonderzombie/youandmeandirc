f809e7e652b55a4b6da3adce0e0f83b36f71d294
Matthew <wonderzombie@gmail.com>
2012-04-04 21:01:59 -0700

    Move the "score" module into its own file.

--- bot.go
+++ bot.go
@@ -1,10 +1,8 @@
 package youandmeandirc
 
 import (
-	"fmt"
 	"log"
 	"math/rand"
-	"regexp"
 	"strings"
 	"time"
 )
@@ -68,62 +66,6 @@ func (bot *IrcBot) onNameListener() (onName Listener) {
 
 		bot.irc.Say(msg.Channel, sayings[choice])
 		return
-	}
-	return
-}
-
-// TODO: move this into its own file.
-// TODO: a way to ask for your score.
-func (bot *IrcBot) scoreListener() (scorer Listener) {
-	type Point struct {
-		Granter string
-		When    time.Time
-		Reason  string
-	}
-
-	type Score struct {
-		Total  int
-		Points []Point
-	}
-
-	scoreMap := make(map[string]Score, 0)
-
-	scorer = func(msg IrcMessage) (fired, trap bool) {
-		if msg.Command != "PRIVMSG" {
-			return
-		}
-
-		if !(strings.Contains(msg.Text, "++") || strings.Contains(msg.Text, "--")) {
-			log.Printf("Is not a score message, skipping.")
-			return
-		}
-		log.Printf("Is possibly a score message.")
-
-		re := regexp.MustCompile("(\\w+)(\\+\\+|\\-\\-)")
-		match := re.FindStringSubmatch(msg.Text)
-		if len(match) == 0 {
-			return
-		}
-
-		nick := match[1]
-		delta := -1
-		if match[2] == "++" {
-			delta = 1
-		}
-		granter := msg.Origin
-		newPoint := Point{Granter: granter, When: time.Now(), Reason: msg.Text}
-		log.Printf("Looks like a score message for %v from %v.\n", nick, granter)
-
-		// TODO: user a pointer instead.
-		score := scoreMap[nick]
-		score.Total += delta
-		score.Points = append(score.Points, newPoint)
-		scoreMap[nick] = score
-
-		out := fmt.Sprintf("%v's score is now %d", nick, score.Total)
-		bot.irc.Say(msg.Channel, out)
-
-		return true, true
 	}
 	return
 }
