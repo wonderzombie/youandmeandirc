ef673394d26956e12039e4d76482fd5602f17cb9
Matthew <wonderzombie@gmail.com>
2012-05-04 14:02:28 -0700

    Remove commented out/useless code.

--- gobot/main.go
+++ gobot/main.go
@@ -28,60 +28,6 @@ func init() {
 	flag.Parse()
 }
 
-// func defaultMain() {
-// 	addr := net.JoinHostPort(*host, *port)
-// 	timeout, _ := time.ParseDuration("1m")
-
-// 	irc := &irclib.IrcConn{
-// 		Username: *username,
-// 		Pass:     *pass,
-// 		Nick:     *nick,
-// 		Realname: "...",
-// 	}
-// 	// TODO(wonderzombie): Fix this so that IrcConn takes a closure, or something
-// 	// which can generate net.Conn items for it.
-// 	conn, _ := net.DialTimeout("tcp", addr, timeout)
-// 	defer conn.Close()
-// 	irc.Connect(conn)
-
-// 	joined := false
-// 	for {
-// 		m, err := irc.Read()
-// 		if err != nil {
-// 			log.Println("Skipping message: ", m.Raw)
-// 			continue
-// 		}
-
-// 		log.Print("<=", m.Raw)
-
-// 		if m.Command == "PING" {
-// 			daemon := m.Params[0]
-// 			irc.Pong(daemon)
-// 			continue
-// 		}
-
-// 		if !joined && m.Origin == *nick && m.Command == CmdMode {
-// 			// We've finished connecting. Join the channel.
-// 			irc.Join(*channel)
-// 			joined = true
-// 		} else if m.Command == CdmPrivmsg && m.Channel == *channel {
-// 			if hasMyName(m.Text) {
-// 				resp := "zzz"
-// 				if strings.Contains(m.Text, "ACTION") {
-// 					resp = "what are you doing"
-// 				} else if m.Origin == "trapro" {
-// 					resp = "trapro sux"
-// 				}
-// 				irc.Say(*channel, resp)
-// 			}
-// 		}
-// 	}
-// }
-
-func botMain() {
-
-}
-
 func main() {
 	log.Println("hello youandmeandirc")
 
